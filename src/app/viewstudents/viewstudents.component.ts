c33b7b69eb6c1a6ee07eb60efa1cd7196d3af37e
MansiMalani <mansimalani0609@gmail.com>
2022-07-08 10:10:35 +0530

    On going

---
+++ src/app/viewstudents/viewstudents.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewstudents',
+  templateUrl: './viewstudents.component.html',
+  styleUrls: ['./viewstudents.component.css']
+})
+export class ViewstudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  name="Aananya"
+
+}
